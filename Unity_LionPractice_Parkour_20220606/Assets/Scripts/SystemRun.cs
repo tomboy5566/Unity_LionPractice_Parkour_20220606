0d1afcae8bf121c1fbbe59acfb94efab603475ae
tomboy5566 <105775195+tomboy5566@users.noreply.github.com>
2022-06-13 12:01:02 +0800

    Update 增加跑步功能

---
+++ Unity_LionPractice_Parkour_20220606/Assets/Scripts/SystemRun.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+namespace KuanLun
+{
+    /// <summary>
+    /// 跑步功能+跳躍功能
+    /// </summary>
+    public class SystemRun : MonoBehaviour
+    {
+        #region 資料:系統所需資料
+        [SerializeField, Header("角色跑步速度"), Tooltip("控制角色跑步速度"), Range(0,100)]
+        private float speedRun = 3.5f;
+        [SerializeField, Header("角色跳躍高度"), Tooltip("控制角色跳躍高度"), Range(0, 3000)]
+        private float heightJump = 350;
+        private Animator ani;
+        private Rigidbody2D rig;
+        #endregion
+        #region 功能:實現系統功能的方法
+
+        #endregion
+        #region 事件:程式入口
+        private void Start()
+        {
+            print("2ㄏ2ㄏ");
+        }
+        #endregion
+
+
+    }
+}
+
+
